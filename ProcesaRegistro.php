6b9495c9915893934192748647b07187c6bee57e
Maria Emilia Ramirez <mariaemiliaramirezgomez@gmail.com>
2018-05-08 21:28:58 -0500

    Validación datos y revisar si coinciden las contraseñas

---
+++ ProcesaRegistro.php
@@ -0,0 +1,36 @@
+<?php
+//ProcesRegistro.php
+	session_start();
+	$arch='registro.html';//Ruta página registro 
+	forEach($_POST as $ind => $ele)
+		$_SESSION[$ind]=$ele;
+	$regex=array(
+	'nombre' => '/^[A-Z][a-z]+/',
+	'contraseña' =>'/(?=^.{10,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/',
+	'contraseña2' =>'/(?=^.{10,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/',
+	'imagen'=> '/^[1-6]$/'
+	);
+	//VALIDACIÓN 1, todos los elementos cumplen con la regex  
+	forEach($_SESSION as $ind => $ele)
+	{
+		$n=preg_match($regex[$ind], $ele);
+		if ($n==0)
+			echo $ele.' es un dato incorrecto <br />';
+		else 
+			$a++;
+	}
+	if ($a<4) 
+		echo '<a href="'.$arch.'" clas="fondo">Regresar </a>';	
+	else 
+	//VALIDACIÓN 2, contraseña1 y contraseña2 son las mismas 
+		if($_SESSION['contraseña']!=$_SESSION['contraseña2'])
+			echo 'La contraseña no coincide<a href="'.$arch.'" clas="fondo">Regresar </a>';
+		else
+			echo'yey. datos validados!';//continuar con el código 
+		
+		
+		
+	//print_r($_SESSION);
+	session_unset();//Solo mientras todos los archivos están separados
+	session_destroy();//No queremos dañar a nuestro buen amigo el servidor 
+?>
