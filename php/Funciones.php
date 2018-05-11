258c6ea189a2a7be2a18a5ac9a1d513ddfd0601e
Maria Emilia Ramirez <mariaemiliaramirezgomez@gmail.com>
2018-05-10 22:22:04 -0500

    Ya no hay error, Dice Hola fulano y puedes ver las imagenes

--- php/Funciones.php
+++ php/Funciones.php
@@ -1,6 +1,7 @@
 <?php
 	session_start();
-	function vaciar ($nom_arch)//sacar todos los registros que haya en un archivo y pasarlo a un array, devuelde el array 
+
+	function vaciar ($nom_arch)//sacar todos los registros que haya en un archivo y pasarlo a un array, devuelde el array
 	{
 		$todos=[];
 		$i=0;
@@ -15,42 +16,46 @@
 		fclose($arch);
 		return $todos;
 	}
+
+
 	function checar_arch ()
 	{
 		$exist=false;
 		$sigue=true;
 		$usuarios=vaciar('..\archivos\usuarios.txt');
-		$nombre=$_SESSION['nombre'];
 		$max=count($usuarios);//la longitud de toooodos los registros, de ahí suma de 3 en 3 para sacar uno por uno
 		for($i=0;$i<$max;$i+=3)
 			$usu_ord[$usuarios[$i]]=[$usuarios[$i+1],$usuarios[$i+2]];
 		foreach($usu_ord as $i=>$elem)//revisar nombre recibido con todos los nombres que hay
-			if($i==$nombre)
+			if($i==$_SESSION['nombre'])
 			{
-				echo 'existe'.'</br>';
+				//echo 'existe'.'</br>';MONITOREAR
 				$exist=true;
 			}
-		//echo $_SESSION['contraseña'].'</br>';
-		//echo $usu_ord[$_SESSION['nombre']][0].'</br>';
-		if ($exist==false)
+		//echo $_SESSION['contraseña'].'</br>';MONITOREAR, YA NO ES NECESARIO MOSTRARLO, CONTRASEÑA FORMULARIO
+		$nombre=$_SESSION['nombre'];//MARCABA UN 'WARNING' PORQUE COVERTIA STRING TO ARRAY, ALGO UN POCO LOCO
+		//echo $usu_ord[$nombre][0].'</br>';MONITOREAR, CONTRASEÑA DEL ARCHIVO
+		if ($exist==false)//GUARDA UN NUEVO USUARIO EN CASO DE QUE NO COINCIDA NINGUN REGISTRO
 		{
 				$arch=fopen('..\archivos\usuarios.txt','a+');
-					fputs($arch,"\r\n".$_SESSION['nombre']);
-					fputs($arch,"\r\n".$_SESSION['contraseña']);
-					fputs($arch,"\r\n".$_SESSION['imagen']);
+
+				fputs($arch,$_SESSION['nombre'].'\n');
+				fputs($arch,$_SESSION['contraseña'].'\n');
+				fputs($arch,$_SESSION['imagen'].'\n');
+
 				fclose($arch);
-				echo 'lo escribo'.'</br>';
 		}
 		if ($exist==true)
 		{
-			if ($_SESSION['contraseña']!=$usu_ord[$_SESSION['nombre']][0]) 
+			if ($_SESSION['contraseña']!=$usu_ord[$_SESSION['nombre']][0])//COMPARA CONTRASEÑA YA QUE EL USUARIO SI ESTÁ REGISTRADO
 			{
 				$sigue=false;
-				echo 'contraseña mal'.'</br>';
+				echo 'Tu contraseña no coincide, intentalo de nuevo'.'</br>';
 			}
 		}
 		return $sigue;
 	}
+
 	function revisa()
 	{
 		if(isset($_COOKIE['nombre']))
@@ -58,5 +63,6 @@
 		else
 			$exist = false;
 		return $exist;
-	} 
+	}
+
 ?>
