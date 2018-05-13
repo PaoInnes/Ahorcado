689af2b2fae7bead2257f4e47641e45f17fa14dd
Maria Emilia Ramirez <mariaemiliaramirezgomez@gmail.com>
2018-05-12 22:56:30 -0500

    esto no es

--- php/juego.php
+++ php/juego.php
@@ -1,49 +1,62 @@
 <?php
 	@include 'Funciones.php';
 	//session_start();
-	echo $_SESSION['p1'];
+	//echo $_SESSION['p1'];
 	if ($_SESSION['p1']=='no')
 	{
-		echo '___________';
-		$letra = 'a';
+		$letra = $_GET['letra'];;
 		$n=preg_match('/^[A-Za-z]$/', $letra);
 		if ($n==1)
 		{
 			$letra=strtoupper ($letra);
 			if ($letra!='X')
 			{
-				$monito=tachar($letra);//Esta es la imagen que va mostrando, también son los errores que lleva
-				if ($monito==3 ||$monito==6 ||$monito==9)//ya tuvo algunos errores
+				tachar($letra);//Esta es la imagen que va mostrando, también son los errores que lleva
+				if ($_SESSION['error']==3 ||$_SESSION['error']==6 ||$_SESSION['error']==9)//ya tuvo algunos errores
 				{
 					$letra=pista((strlen($_SESSION['goodpal'])),$_SESSION['goodpal']);//generar la primera pista
-					$monito=tachar($letra);
+					tachar($letra);
+					echo 'Te ayudamos con una pequeña pista para que logres salvar a tu amigo :)';
 				}
 			}
-			// else 
-				// echo 'perdiste muejajajajja';
-			
+			else
+				$_SESSION['error']++;
+
 		}
 	}
-	
 	if ($_SESSION['p1']=='si')
 	{
-		echo '________';
 		primeraVez();
 		$letra=pista((strlen($_SESSION['goodpal'])),$_SESSION['goodpal']);//generar la primera pista
-		$monito=tachar($letra);
+		tachar($letra);
 	}
-	
-	$aciertos=substr_count($_SESSION['badpal'],'x');
-	if($monito<=10 && $aciertos<0 )
+	$aciertos=substr_count($_SESSION['badpal'],'X');
+	if($_SESSION['error']<=10 && $aciertos>0 )
 	{
+		echo '<img src="..\archivos\ahor'.$_SESSION['error'].'.jpg" width="20%" height="40%"/>';
 		echo '<form action="juego.php" method="get">';
+			//echo $_SESSION['goodpal'].'<br />';
+			echo $_SESSION['badpal'].'<br />';
 			echo '<input type="text" name="letra" maxlength="2" required pattern="^[A-Za-z]$" />';
 			echo '<input type="submit" />';
 		echo '</form>';
 	}
 	else
-		echo 'perdiste muejajajajja';
-	//session_unset();//Solo mientras todos los archivos están separados
-  	//session_destroy();//No queremos dañar a nuestro buen amigo el servidor
-		
+	{
+		if ($_SESSION['error']>10)
+			{
+			echo '<p>Vaya... Estuviste muy cerca de salvarlo.</p>';
+			echo '<p>Yo soy intimo amigo del rey, si gustas puedo pedirle que te de otra oportunidad.</p>';
+			}
+		else
+			{
+			echo '<p>¡WOW! Eres muy bueno en esto. ¿No te gustaría salvar a más gente del reino? Con esa habilidad que tienes será pan comido.</p>';
+			}
+		echo '<p>¿Qué dices '.$_SESSION['nombre'].', aceptas? No olvides que está en juego la vida de alguien.</p>';
+		echo '<form action="respuesta.php" method="post">';
+			echo '¡Claro que si! <input type="radio" name="respuesta" value="1" /><br /><br />';
+			echo 'Será para la otra <input type="radio" name="respuesta" value="2" /><br /><br />';
+			echo '<input type="submit"  />';
+		echo '</form>';
+	}
 ?>
